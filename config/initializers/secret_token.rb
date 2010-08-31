6e62eb3c27aff8592ee903d7765796867b58c53a
Chris Collins <chris@collins.id.au>
2010-08-31 21:21:10 +1000

    initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billspike::Application.config.secret_token = '06bb06291729471685ca2ab9b79d6c9f2d09d65bceef885dfd2e3271cb7d853446b6f441ebf0c193d92db7c69debe1079bfc3a1442d644ff94d849c6ee1a08f7'
